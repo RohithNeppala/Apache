a1f130a1972be9a432f6df7957a7ce9d6ca86dd2
RohithNeppala <149653740+RohithNeppala@users.noreply.github.com>
2024-01-11 23:37:50 +0530

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -1,4 +1,5 @@
 amazon-linux-extras install java-openjdk11 -y
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.tar.gz
-tar -zxvf apache-tomcat9.0.85.tar.gz
-cd bin
+tar -zxvf apache-tomcat-9.0.85.tar.gz
+cd apache-tomcat-9.0.85
+cd bin/
